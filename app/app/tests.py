2d87c667f936835e0a8b7fb106c4b73f62dc69b0
Zhandos <yernazarovz@gmail.com>
2021-06-15 14:03:38 +0600

    Added custom user model

--- app/app/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-from app.calc import add
-class CalcTests(TestCase):
-    def test_1(self):
-        self.assertEqual(add(3,4),7)
---
+++ app/core/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        if not email:
+            raise ValueError("Users must have an email address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password) #separately because password needs to be encrypted
+        user.save(using=self._db) #using... is to support multiple databases
+        return user
+
+    def create_superuser(self, email, password=None): #no worry about extra fields, as it is created by command line
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=264, unique=True) #unique so to create one user for one email
+    name = models.CharField(max_length=264)
+    is_active = models.BooleanField(default=True) #is user is active/online or not
+    is_staff = models.BooleanField(default=False) #if staff, create user with special command
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
